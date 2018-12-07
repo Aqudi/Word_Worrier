b440f85174c984c1df1ac2cf600825ac10a3abe9
gjdigj145 <gjdigj>
2018-12-07 12:41:02 +0900

    Searcher클래스 Word클래스 클래스명 변경 반영 getWordInformation메소드 wordName변수에 Word클래스의 extractHangul적용 검색결과와 검색키워드의 불일치 발생시 단어길이를 기준으로 재검색 & 결과반환

--- lib/Searcher.py
+++ lib/Searcher.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import configparser
-from word import Word
+from Word import Word
 
 
 class Searcher:
@@ -10,34 +10,52 @@ class Searcher:
         config.read("config.ini")
         self.API_KEY = config.get('Default', 'api_key')
 
-    #
     def getWordInformation(self, word, start=1):
         BASE_URL = "https://opendict.korean.go.kr/api/search"
         parameters = {"key": self.API_KEY, 'target_type': 'search',
                       'part': 'word', 'q': word,
-                      'sort': 'dict', 'start': start,
-                      'num': 10,
+                      'sort': 'dict', 'start': start, 'num': 10,
                       }
 
         with requests.Session() as s:
-            request = s.get(BASE_URL, params=parameters)
-            if request.status_code == 200 and request.ok:
-                soup = BeautifulSoup(request.text, 'html.parser')
-                wordName = soup.find("word").string
-                wordDefinition = soup.find("definition").string
-                if wordName is None or wordDefinition is None:
-                    return "error: 없는데이터"
+            for i in range(1, 5):
+                # 결과가 예상한 것과 다를 경우 표제어의 번호를 바꿔가며 재시도
+                parameters[start] = i
+                print("{}번째 요청".format(i))
+
+                # 우리말샘 API에 검색결과를 요청
+                request = s.get(BASE_URL, params=parameters)
+                if request.status_code == 200 and request.ok:
+                    soup = BeautifulSoup(request.text, 'html.parser')
+
+                    # 한글이외의 내용들 제거 (예시) 맏-손자 -> 맏손자
+                    wordName = Word.extractHangul(soup.find("word").string)
+                    wordDefinition = soup.find("definition").string
+
+                    # 검색결과와 검색 키워드가 달라질경우 : 단어의 길이 우선적으로 검사 
+                    if len(wordName) != len(word):
+                        continue
+                    elif wordName is None or wordDefinition is None:
+                        return "error: 없는데이터"
+                    else:
+                        return wordName + ": " + wordDefinition
                 else:
-                    return wordName + ": " + wordDefinition
-            else:
-                return "error: Request에러"
+                    return "error: Request에러"
+
+            # 제대로된 결과를 얻지 못했을 경우
+            return "error: 없는데이터"
 
 
 if __name__ == "__main__":
     d = Searcher()
+    
+    # 원하는 단어 검색 테스트
     word = input("검색하고 싶은 단어를 입력해주세요 : ")
     result = d.getWordInformation(word)
     print(result)
+    print()
+    
+    # 랜덤단어 검색 테스트
     w = Word()
     result = d.getWordInformation(w.getRandomWord())
     print(result)
