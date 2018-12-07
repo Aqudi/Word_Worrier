a247adec6eb4d8385305e5002b8fb61a5c446fb7
gjdigj145 <gjdigj>
2018-12-07 12:44:21 +0900

    Word클래스 출력문 변경 decomposeHangul메소드 static으로 변경, self에러 수정

--- lib/Word.py
+++ lib/Word.py
@@ -11,7 +11,7 @@ class Word:
         fileName = topDir / "Data" / "Words.txt"
         with codecs.open(fileName, 'r', "utf-8") as f:
             self.words = [x.rstrip() for x in f.readlines()]
-            print("단어의 수 : ", len(self.words))
+            print("단어DB의 크기 : ", len(self.words))
 
     def getRandomWord(self, length=2):
         word = ""
@@ -27,7 +27,8 @@ class Word:
         result = hangul.sub('', string)  # 한글과 띄어쓰기를 제외한 모든 부분을 제거
         return result
 
-    def decomposeHangul(self, string):
+    @staticmethod
+    def decomposeHangul(string):
         # 계산식 출처: https: // github.com / neotune / python - korean - handler
         # 라이선스: MIT라이선스
 
@@ -37,7 +38,7 @@ class Word:
         CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
         result = []
-        string = self.extractHangul(string)
+        string = Word.extractHangul(string)
 
         for char in string:
             char_code = ord(char) - BASE_CODE
