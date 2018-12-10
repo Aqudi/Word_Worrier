d8187e616edcd856d2b2db211fc65bd9aedd6c3c
gjdigj145 <gjdigj>
2018-12-10 14:33:23 +0900

    Card클래스, Chracter클래스 aa(auto attack)추가, get information 리턴형식 변경 set_card_pool 메소드 : 카드 학률 풀 설정 set_random_pool_value 메소드 : 카드의 유형, 이름, 값 설정 get_information 메소드 : 카드의 정보 반환

---
+++ lib/card.py
@@ -0,0 +1,67 @@
+from random import choice, randrange
+
+
+class Card:
+    def __init__(self):
+        # 카드의 유형, 이름, 값
+        self.card_type = ""
+        self.card_name = ""
+        self.card_stats_value = 0
+
+        # 카드유형 공격, 스탯업그레이드
+        self.CARD_TYPE_LIST = ("ATTACK", "STAT")
+
+        # 공격, 스탯카드의 종류
+        self.STATS_LIST = ("HP", "AP", "AC", "CC", "AA")
+        self.ATTACKS_LIST = ("TRIPLE", "DOUBLE", "NORMAL")
+
+        # 카드 풀 value : 기본세팅
+        type_pool_values = [3, 2]
+        stats_pool_values = [4, 3, 4, 2, 6]
+        attacks_pool_values = [1, 3, 6]
+        
+        # 카드 풀 세팅
+        self.card_pool = {"TYPE": [],
+                          "STAT": [],
+                          "ATTACK": []}
+        self.card_pool_list = {"TYPE": self.CARD_TYPE_LIST,
+                               "STAT": self.STATS_LIST,
+                               "ATTACK": self.ATTACKS_LIST}
+        self.card_pool_values = {"TYPE": type_pool_values,
+                                 "STAT": stats_pool_values,
+                                 "ATTACK": attacks_pool_values}
+        self.set_card_pool()
+        
+        # 실제 카드 정보 저장
+        self.set_random_pool_value()
+
+    # 카드 확률 풀 설정
+    def set_card_pool(self, keys=None):
+        if keys is None:
+            keys = self.card_pool.keys()
+        for key in keys:
+            for name, value in zip(self.card_pool_list[key], self.card_pool_values[key]):
+                self.card_pool[key] += [name] * value
+
+    def set_random_pool_value(self):
+        value_scope = {}
+        value_list = [(20, 40), (10, 20), (2, 4), (2, 4), (2, 5), (3, 4), (2, 3), (1, 2)]
+        for name, value in zip(self.STATS_LIST + self.ATTACKS_LIST, value_list):
+            value_scope.update({name: value})
+        self.card_type = choice(self.card_pool["TYPE"])
+        self.card_name = choice(self.card_pool[self.card_type])
+        self.card_stats_value = randrange(value_scope[self.card_name][0], value_scope[self.card_name][1],)
+
+    def get_information(self):
+        information = [("TYPE", self.card_type), ("NAME", self.card_name), ("VALUE", self.card_stats_value)]
+        for tag, value in information:
+            print(tag + " : ", value)
+        return information
+
+
+if __name__ == "__main__":
+    c = Card()
+    for i in range(100):
+        c.set_random_pool_value()
+        c.get_information()
+
