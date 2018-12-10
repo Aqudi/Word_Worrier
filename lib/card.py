f7e355f5b8c2c7f3d34e9f198bf437fffbb6e782
gjdigj145 <gjdigj>
2018-12-10 22:09:41 +0900

    Card클래스 getter추가 HEAL카드 추가

--- lib/card.py
+++ lib/card.py
@@ -9,27 +9,32 @@ class Card:
         self.card_stats_value = 0
 
         # 카드유형 공격, 스탯업그레이드
-        self.CARD_TYPE_LIST = ("ATTACK", "STAT")
+        self.CARD_TYPE_LIST = ("ATTACK", "STAT", "HEAL")
 
         # 공격, 스탯카드의 종류
         self.STATS_LIST = ("HP", "AP", "AC", "CC", "AA")
         self.ATTACKS_LIST = ("TRIPLE", "DOUBLE", "NORMAL")
+        self.HEALS_LIST = ("MIRACLE", "HOLY", "CURE")
 
         # 카드 풀 value : 기본세팅
-        type_pool_values = [3, 2]
+        type_pool_values = [3, 2, 2]
         stats_pool_values = [4, 3, 4, 2, 6]
         attacks_pool_values = [1, 3, 6]
-        
+        heals_pool_values = [1, 5, 13]
+
         # 카드 풀 세팅
         self.card_pool = {"TYPE": [],
                           "STAT": [],
-                          "ATTACK": []}
+                          "ATTACK": [],
+                          "HEAL": []}
         self.card_pool_list = {"TYPE": self.CARD_TYPE_LIST,
                                "STAT": self.STATS_LIST,
-                               "ATTACK": self.ATTACKS_LIST}
+                               "ATTACK": self.ATTACKS_LIST,
+                               "HEAL": self.HEALS_LIST}
         self.card_pool_values = {"TYPE": type_pool_values,
                                  "STAT": stats_pool_values,
-                                 "ATTACK": attacks_pool_values}
+                                 "ATTACK": attacks_pool_values,
+                                 "HEAL": heals_pool_values}
         self.set_card_pool()
         
         # 실제 카드 정보 저장
@@ -42,15 +47,26 @@ class Card:
         for key in keys:
             for name, value in zip(self.card_pool_list[key], self.card_pool_values[key]):
                 self.card_pool[key] += [name] * value
+        #print(self.card_pool)
 
     def set_random_pool_value(self):
         value_scope = {}
-        value_list = [(20, 40), (10, 20), (2, 4), (2, 4), (2, 5), (3, 4), (2, 3), (1, 2)]
-        for name, value in zip(self.STATS_LIST + self.ATTACKS_LIST, value_list):
+        value_list = [(20, 40), (10, 20), (2, 4), (2, 4), (2, 5), (3, 4), (2, 3), (1, 2), (5, 8), (10, 12), (17, 20)]
+        for name, value in zip(self.STATS_LIST + self.ATTACKS_LIST + self.HEALS_LIST, value_list):
             value_scope.update({name: value})
+        #print(value_scope)
         self.card_type = choice(self.card_pool["TYPE"])
         self.card_name = choice(self.card_pool[self.card_type])
         self.card_stats_value = randrange(value_scope[self.card_name][0], value_scope[self.card_name][1],)
+
+    def get_type(self):
+        return self.card_type
+
+    def get_name(self):
+        return self.card_name
+
+    def get_value(self):
+        return self.card_stats_value
 
     def get_information(self):
         information = [("TYPE", self.card_type), ("NAME", self.card_name), ("VALUE", self.card_stats_value)]
